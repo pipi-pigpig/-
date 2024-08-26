3a9be2953be143c6b0b1b08f6370481e1ef3c226
LXC562 <13437055681@139.com>
2024-08-26 15:24:42 +0800

    测试--03

---
+++ src/main/java/com/itheima/test/Test.java
@@ -0,0 +1,83 @@
+package com.itheima.test;
+
+
+
+import com.itheima.Mapper.UserMapper;
+import com.itheima.pojo.User;
+import com.itheima.pojo.model.MessageModel;
+import com.itheima.util.GetSqlSession;
+import com.itheima.util.StringUtil;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+public class Test {
+    public static MessageModel userLogin(String uname, String upwd) throws IOException {
+        MessageModel messageModel = new MessageModel();
+        //回显数据
+        User u = new User();
+        u.setUsername(uname);
+        u.setPassword(upwd);
+        messageModel.setObject(u);
+
+        if(StringUtil.isEmpty(uname) || StringUtil.isEmpty(upwd)){
+            messageModel.setCode(0);
+            messageModel.setMsg("用户姓名和密码不能为空！");
+
+            return messageModel;
+        }
+
+
+        // 加载mybatis的核心配置文件，获取SqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = GetSqlSession.getSession();
+        UserMapper userMapper = session.getMapper(UserMapper.class);
+        //------------------------------  bug  ---------------------------------------------------
+        User user = userMapper.selectUser(uname);
+
+
+        if (user == null){
+            messageModel.setCode(0);
+            messageModel.setMsg("用户不存在！");
+            return messageModel;
+        }
+
+        if (!upwd.equals(user.getPassword())){
+            messageModel.setCode(0);
+            messageModel.setMsg("用户密码不正确！");
+            return messageModel;
+        }
+
+        messageModel.setObject(user);
+
+        return messageModel;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+
+        MessageModel messageModel = userLogin("user1", "password1");
+
+        if(messageModel.getCode()==1){
+            System.out.println(messageModel.getMsg());
+            System.out.println(messageModel.getObject());
+            System.out.println("11111");
+        }
+        else{
+            System.out.println(messageModel.getMsg());
+            System.out.println(messageModel.getObject());
+            System.out.println("00000");
+        }
+    }
+    }
+
+
+
+
